2f5c1a9b50619630cb744c82c030458419736b17
joma74 <dev-mails@gmx.net>
2018-04-27 00:21:01 +0200

    chore: added ts' jsdoc for feature of Microsoft/TypeScript#14377

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,19 @@
-
 // @ts-ignore
 import renderFnkt from "@home/app.html?style=@home/app.css";
 
-/* eslint-disable no-new */
-export default renderFnkt({
+
+/** @type {import("vue").ComponentOptions} */
+let vueComponentOptions = {
+  name: "SPARoot",
   mounted() {
-    document.documentElement.replaceChild(this.$el, document.body);
+    /** @type {import("vue").default} */
+    // @ts-ignore
+    const compInst = this;
+    if(compInst.$el instanceof HTMLBodyElement)
+      document.documentElement.replaceChild(compInst.$el, document.body);
+    else
+      console.error(`this.el having a value of >>${compInst.$el}<< should be an HTMLBodyElement but is not`);
   }
-});
+};
+
+export default renderFnkt(vueComponentOptions);
